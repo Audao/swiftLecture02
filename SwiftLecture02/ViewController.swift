358314397d15d3d754769e42dc4a95e9c216c403
Audao <audao@163.com>
2018-07-22 15:55:19 +0800

    no.3

--- SwiftLecture02/ViewController.swift
+++ SwiftLecture02/ViewController.swift
@@ -21,6 +21,24 @@ class ViewController: UIViewController {
         let fullName = name + "chen"
         lbName?.text = fullName
         print(fullName)
+        //Array
+        let english = ["A","B","C"]
+        var ints : [Int] = [1,2,3]
+        var floats : Array<Float> = Array(repeating: 0, count: 5)
+        
+        ints.sort()
+        for index in 0..<3{
+            print("\( english[index])")
+        }
+        for str in english{
+            print(str)
+        //Dictionary
+            let 啊 = [1:"A",2:"C",3:"B"]
+            for(a,b) in 啊 {
+                print("\( a ):\( b )")
+            }
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
